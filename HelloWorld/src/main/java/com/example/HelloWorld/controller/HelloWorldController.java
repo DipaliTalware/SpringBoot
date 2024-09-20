e70cfcb7451cb685c15eb15c651f87c6e846178f
DipaliTalware <d.talware@gmail.com>
2024-09-20 11:07:18 +0200

    Use Path variable

--- HelloWorld/src/main/java/com/example/HelloWorld/controller/HelloWorldController.java
+++ HelloWorld/src/main/java/com/example/HelloWorld/controller/HelloWorldController.java
@@ -1,13 +1,16 @@
 package com.example.HelloWorld.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
-    @GetMapping("/api/hello")
-    public String getHelloWorld(){
-        return "Hello World!";
+    @GetMapping("/api/hello/{name}")
+    public String getHelloWorld(@PathVariable String name){
+        StringBuilder s = new StringBuilder();
+        s.append("Hello ").append(name).append("!");
+        return s.toString();
     }
 }
